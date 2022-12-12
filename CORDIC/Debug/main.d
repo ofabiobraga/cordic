b66a405f6e7d005d4dddd9139bae7ae84c34b221
Fábio Braga <github@fabiobraga.net>
2022-12-12 17:55:29 -0300

    Initial commit

---
+++ CORDIC/Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdio.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: ../cordic.h
+main.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdint.h
+
+../main.c:
+
+C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdio.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/linkage.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdarg.h:
+
+../cordic.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdint.h:
+
---
+++ CORDIC/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../VC5502.cmd 
+
+C_SRCS += \
+../cordic.c \
+../main.c 
+
+C_DEPS += \
+./cordic.d \
+./main.d 
+
+OBJS += \
+./cordic.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"cordic.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"cordic.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../cordic.c" \
+"../main.c" 
+
+
