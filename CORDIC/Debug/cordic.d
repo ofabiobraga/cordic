e33060e6b0f07c7bed076f97503160fc7ebbf786
Fábio Braga <github@fabiobraga.net>
2022-12-13 05:48:01 -0300

    Adicionado arquivos da última compilação e execução

--- CORDIC/Debug/cordic.d
+++ CORDIC/Debug/cordic.d
@@ -1,40 +1,10 @@
 # FIXED
 
 cordic.obj: ../cordic.c
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdio.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/linkage.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdarg.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/math.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/float.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/access.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/elfnames.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/mathf.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/mathl.h
-cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/unaccess.h
 cordic.obj: C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdint.h
 cordic.obj: ../cordic.h
 
 ../cordic.c:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdio.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/linkage.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdarg.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/math.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/float.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/access.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/elfnames.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/mathf.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/mathl.h:
-
-C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/unaccess.h:
 
 C:/ti/ccs1210/ccs/tools/compiler/c5500_4.4.1/include/stdint.h:
 
